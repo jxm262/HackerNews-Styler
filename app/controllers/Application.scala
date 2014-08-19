7f9c32bf9e6d7ff339fe5ae597daf019d96953a9
Justin Maat <jxm262@gmail.com>
2014-08-19 00:33:21 -0400

    Adding some exploratory code to get manipulate the dom

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,43 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import org.jsoup.nodes.Document
+import org.jsoup.Jsoup
+import scala.concurrent.Future
+import play.api.libs.ws.WS
+import play.api.libs.ws.Response
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.collection.JavaConversions._
 
 object Application extends Controller {
 
+  def updateHtml(body: String): String = {
+    val doc = Jsoup.parse(body);
+
+    doc.select("link").map(e => e.attr("href", "https://news.ycombinator.com/" + e.attr("href")))
+    doc.select("img").map(e => e.attr("src", "https://news.ycombinator.com/" + e.attr("src")))
+    
+    for{
+      e <- doc.select("a")
+      if(!e.attr("href").contains("http://") || !e.attr("href").contains("https://"))
+    } yield(e.attr("href", "https://news.ycombinator.com/" + e.attr("href")))
+    
+    doc.select("head").append("<link type='text/css' href='https://raw.github.com/Primigenus/Cleaner-Hacker-News/master/cleaner-hn.css' rel='stylesheet'>")
+
+    doc.toString()
+  }
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val myFunction: Future[Response] = WS.url("https://news.ycombinator.com/").get()
+
+    Async {
+      myFunction.map {
+        resp =>
+          {
+            Ok(updateHtml(resp.body)).as(HTML)
+          }
+      }
+    }
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ name := "HackerNews-Styler"
 version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
+  "org.jsoup" % "jsoup" % "1.7.3",
   jdbc,
   anorm,
   cache
