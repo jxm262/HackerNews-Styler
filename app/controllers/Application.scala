db8e6ac600e2f36fb670250c0490e27d20ebea8e
Justin Maat <jxm262@gmail.com>
2014-08-20 00:37:20 -0400

    Adding more exploratory code to style the comments pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,15 +17,44 @@ object Application extends Controller {
 
     doc.select("link").map(e => e.attr("href", "https://news.ycombinator.com/" + e.attr("href")))
     doc.select("img").map(e => e.attr("src", "https://news.ycombinator.com/" + e.attr("src")))
-    
-    for{
+
+    for {
       e <- doc.select("a")
-      if(!e.attr("href").contains("http://") || !e.attr("href").contains("https://"))
-    } yield(e.attr("href", "https://news.ycombinator.com/" + e.attr("href")))
-    
+      if (!(e.attr("href").contains("http://") || e.attr("href").contains("https://")))
+    } yield {
+      if (e.attr("href").contains("item?")) e.attr("href", "http://localhost:9000/" + e.attr("href"))
+      else e.attr("href", "https://news.ycombinator.com/" + e.attr("href"))
+    }
+
     doc.select("head").append("<link type='text/css' href='https://raw.github.com/Primigenus/Cleaner-Hacker-News/master/cleaner-hn.css' rel='stylesheet'>")
 
     doc.toString()
+  }
+  
+  def updateHtml2(body: String): String = {
+    val doc = Jsoup.parse(body);
+
+    doc.select("head").append("<link type='text/css' href='https://raw.github.com/Primigenus/Cleaner-Hacker-News/master/cleaner-hn.css' rel='stylesheet'>")
+
+    doc.toString()
+  }  
+
+  def item(id: Option[Long]) = Action {
+    id match {
+      case Some(x) => {
+        val myFunction: Future[Response] = WS.url("https://news.ycombinator.com/item?id=" + x).get()
+
+        Async {
+          myFunction.map {
+            resp =>
+              {
+                Ok(updateHtml2(resp.body)).as(HTML)
+              }
+          }
+        }
+      }
+      case None => Ok("page not found...")
+    }
   }
 
   def index = Action {
