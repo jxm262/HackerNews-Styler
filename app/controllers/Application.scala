14d63cab33e12d1014e3476cc8f2657511aecf25
Justin <jxm262@gmail.com>
2014-09-06 15:08:58 -0400

    Updating to include all potential routes (except for user logins)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -22,51 +22,35 @@ object Application extends Controller {
       e <- doc.select("a")
       if (!(e.attr("href").contains("http://") || e.attr("href").contains("https://")))
     } yield {
-      if (e.attr("href").contains("item?")) e.attr("href", "http://localhost:9000/" + e.attr("href"))
-      else e.attr("href", "https://news.ycombinator.com/" + e.attr("href"))
+      if (e.attr("href").contains("?")) e.attr("href", "http://localhost:9000/" + e.attr("href"))
+      else e.attr("href", "http://localhost:9000/" + e.attr("href"))
     }
 
     doc.select("head").append("<link type='text/css' href='https://raw.github.com/Primigenus/Cleaner-Hacker-News/master/cleaner-hn.css' rel='stylesheet'>")
-
+    doc.select(".pagetop").last().parent().after("<td style='text-align:right;padding-right:4px;'><span class='pagetop'><select><option value='volvo'>Volvo</option><option value='saab'>Saab</option><option value='opel'>Opel</option><option value='audi'>Audi</option></select></span></td>");
+    
     doc.toString()
   }
-  
-  def updateHtml2(body: String): String = {
-    val doc = Jsoup.parse(body);
 
-    doc.select("head").append("<link type='text/css' href='https://raw.github.com/Primigenus/Cleaner-Hacker-News/master/cleaner-hn.css' rel='stylesheet'>")
-
-    doc.toString()
-  }  
-
-  def item(id: Option[Long]) = Action {
+  def lookup(route: String, id: Option[String]) = Action {
     id match {
       case Some(x) => {
-        val myFunction: Future[Response] = WS.url("https://news.ycombinator.com/item?id=" + x).get()
+        val myFunction: Future[Response] = WS.url("https://news.ycombinator.com/" + route + "?id=" + x).get()
 
         Async {
-          myFunction.map {
-            resp =>
-              {
-                Ok(updateHtml2(resp.body)).as(HTML)
-              }
-          }
+          myFunction.map(resp => Ok(updateHtml(resp.body)).as(HTML));
         }
       }
       case None => Ok("page not found...")
     }
   }
 
-  def index = Action {
-    val myFunction: Future[Response] = WS.url("https://news.ycombinator.com/").get()
+  def index(name: String) = Action {
+    println("name: " + name)
+    val myFunction: Future[Response] = WS.url("https://news.ycombinator.com/" + name).get()
 
     Async {
-      myFunction.map {
-        resp =>
-          {
-            Ok(updateHtml(resp.body)).as(HTML)
-          }
-      }
+      myFunction.map(resp => Ok(updateHtml(resp.body)).as(HTML))
     }
   }
 
