87906d34ce907d944d83a2b5672a00a4b64d2ecb
Justin <jxm262@gmail.com>
2014-09-15 16:20:29 -0400

    Fixing issues with Drop Down selector in UI

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,6 +12,53 @@ import scala.collection.JavaConversions._
 
 object Application extends Controller {
 
+  val appRoot = "jmaat.me"
+    
+  val style = """
+    <link id='style' type='text/css' href='' rel='stylesheet'>
+  """
+    
+  val styles = """
+  	<td style='text-align:right;padding-right:4px;'>
+  		<span class='pagetop'>
+  			<select id='styles' onchange='changeStyle()'>
+  				<option value='hn/assets/stylesheets/cleaner-hn.css' selected='selected'>Cleaner-Hacker-News</option>
+  				<option value='hn/assets/stylesheets/comfy-helvetica.css'>Comfy-Helvetica</option><!--https://comfy-helvetica.jottit.com/user-css -->
+  				<option value='hn/assets/stylesheets/georgify.css'>Georgify</option><!--https://userstyles.org/styles/46180/georgify-for-hacker-news -->
+  				<option value='hn/assets/stylesheets/solarized.css'>Solarized</option><!--https://userstyles.org/styles/76042/hacker-news-neue -->
+  				<option value='hn/assets/stylesheets/neue.css'>Neue</option><!--https://userstyles.org/styles/76042/hacker-news-neue -->
+  			</select>
+  		</span>
+  	</td> 
+  """
+    
+  val styleSelector = """
+  	<script>
+    	var initStyle = (localStorage.getItem('style') !== null) ? localStorage.getItem('style') : 'hn/assets/stylesheets/cleaner-hn.css';
+    	changeStyle(initStyle);
+    
+    	function changeStyle(newStyle){
+    		var style;
+    
+    		if(typeof newStyle !== 'undefined') {
+    			style = newStyle;
+  			} else{
+  				var e = document.getElementById('styles');
+  				style = e.options[e.selectedIndex].value;
+  			}
+    		
+    		document.getElementById('style').href = "http://""" + appRoot + """/" + style;
+    		localStorage.setItem('style', style);
+  		}	
+  	</script>
+  """
+    		
+  val resetOptions = """
+    <script>
+    	document.getElementById('styles').value = localStorage.getItem('style');
+    </script>
+  """
+    
   def updateHtml(body: String): String = {
     val doc = Jsoup.parse(body);
 
@@ -22,13 +69,16 @@ object Application extends Controller {
       e <- doc.select("a")
       if (!(e.attr("href").contains("http://") || e.attr("href").contains("https://")))
     } yield {
-      if (e.attr("href").contains("?")) e.attr("href", "http://localhost:9000/" + e.attr("href"))
-      else e.attr("href", "http://localhost:9000/" + e.attr("href"))
+      if (e.attr("href").contains("?")) e.attr("href", "http://" + appRoot + "/hn/" + e.attr("href"))
+      else e.attr("href", "http://" + appRoot + "/hn/" + e.attr("href"))
     }
-
-    doc.select("head").append("<link type='text/css' href='https://raw.github.com/Primigenus/Cleaner-Hacker-News/master/cleaner-hn.css' rel='stylesheet'>")
-    doc.select(".pagetop").last().parent().after("<td style='text-align:right;padding-right:4px;'><span class='pagetop'><select><option value='volvo'>Volvo</option><option value='saab'>Saab</option><option value='opel'>Opel</option><option value='audi'>Audi</option></select></span></td>");
     
+    doc.select("head").append(style)
+    doc.select("head").append(styleSelector)
+    doc.select(".pagetop").last().parent().remove()
+    doc.select(".pagetop").last().parent().after(styles)
+    doc.select("body").append(resetOptions)
+
     doc.toString()
   }
 
@@ -46,12 +96,11 @@ object Application extends Controller {
   }
 
   def index(name: String) = Action {
-    println("name: " + name)
     val myFunction: Future[Response] = WS.url("https://news.ycombinator.com/" + name).get()
 
     Async {
       myFunction.map(resp => Ok(updateHtml(resp.body)).as(HTML))
     }
   }
-
+  
 }
